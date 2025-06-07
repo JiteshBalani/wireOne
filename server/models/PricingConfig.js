fe9707515f67e959d28f0a14d33bf34f80b2ce9b
JiteshBalani <balani.jitesh911@gmail.com>
2025-06-07 17:20:23 +0530

    server - PricingConfig schema built.

---
+++ server/models/PricingConfig.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const Days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const DistanceBasedPriceSchema = new mongoose.Schema({
+    day: { type: String, enum: Days },
+    uptoKms: Number,
+    price: Number,
+});
+
+const DistanceAdditionalPriceSchema = new mongoose.Schema({
+    afterKms: Number,
+    pricePerKm: Number,
+});
+
+const TimeMultiplierFactorScema = new mongoose.Schema({
+    fromMinutes: Number,
+    toMinutes: Number,
+    multiplier: Number,
+});
+
+const WaitingChargeSchema = new mongoose.Schema({
+    afterMinutes: Number,
+    chargePerMinute: Number,
+});
+
+const PricingConfigSchema = new mongoose.Schema({
+    name: String,
+    active: {type: Boolean, default: false},
+    dbp: [DistanceBasedPriceSchema],
+    dap: [DistanceAdditionalPriceSchema],
+    tmf: [TimeMultiplierFactorScema],
+    wc: [WaitingChargeSchema],
+    createdAt: {type: Date, default: Date.now},
+});
+
+module.exports = mongoose.model("PricingConfig", PricingConfigSchema);
