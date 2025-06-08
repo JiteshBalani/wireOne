b3228edfcb43041f98e09452d968be94b64703e0
JiteshBalani <balani.jitesh911@gmail.com>
2025-06-08 08:17:26 +0530

    CORS error in deployment.

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const configRoutes = require("./routes/config");
 const app = express();
 
 const frontendURL = process.env.FRONTEND_URL || 'http://localhost:5173';
+app.use(express.json());
 
 app.use(cors({
     origin: ['http://localhost:5173', 'https://wireone.vercel.app'], 
@@ -17,7 +18,6 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
 app.use("/api", pricingRoutes);
 app.use("/api/configs", configRoutes);
 
