f55e4c3691dead7b4f78c28cdc53aa46dbf8de8c
JiteshBalani <balani.jitesh911@gmail.com>
2025-06-08 09:47:37 +0530

    Farelogic spelling corrected in baseUrl frontend.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://farelofic.onrender.com',
+  baseURL: 'https://farelogic.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
