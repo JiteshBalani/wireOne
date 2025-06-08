7d45dc90ee615934b9c95006fd72d64f52ce08ed
JiteshBalani <balani.jitesh911@gmail.com>
2025-06-08 09:44:48 +0530

    Hardcoded baseUrl in frontend code.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: 'https://farelofic.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
